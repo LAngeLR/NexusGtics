a5897a0eed725dbd45afbeb616fcdbe97b87b17e
RommelGaray <a20200241@pucp.edu.pe>
2023-09-05 18:21:41 -0500

    Error en la configuración de estilos - pipipi

---
+++ NexusGtics/src/main/java/com/example/nexusgtics/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.nexusgtics.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/login")
+    public String paginaPrincipal(){
+        return "Sistema/login";
+    }
+
+    @GetMapping("/registro")
+    public String registro(){
+        return "Sistema/registro";
+    }
+
+
+    @GetMapping("/plantilla")
+    public String login(){
+        return "login";
+    }
+}
