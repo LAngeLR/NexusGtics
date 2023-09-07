713752911b536c61d8c86761f67dc32e444c2328
David Llanos <david.llanos@pucp.edu.pe>
2023-09-07 02:06:09 -0500

    Analista Despliegue-Sitios

---
+++ NexusGtics/src/main/java/com/example/nexusgtics/controllers/AnalistaDespController.java
@@ -0,0 +1,33 @@
+package com.example.nexusgtics.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/analistaDespliegue")
+public class AnalistaDespController {
+
+    @GetMapping("/")
+    public String paginaPrincipal(){
+        return "AnalistaDespliegue/analistaDespliegue";
+    }
+
+    @GetMapping("/listaSitio")
+    public String listaSitio(){
+        return "AnalistaDespliegue/despliegueListaSitio";
+    }
+
+    @GetMapping("/editarSitio")
+    public String editarSitio(){
+        return "AnalistaDespliegue/despliegueEditarSitio";
+    }
+    @GetMapping("/listaEquipo")
+    public String listaEquipo(){
+        return "AnalistaDespliegue/despliegueListaEquipos";
+    }
+    @GetMapping("/verEquipo")
+    public String verEquipo(){
+        return "AnalistaDespliegue/despliegueVerEquipos";
+    }
+}
