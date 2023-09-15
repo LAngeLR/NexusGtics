bff9b74a57de5cc4c1e3e46cfac06dbe1397808c
LuisAngel <a20192832@pucp.edu.pe>
2023-09-15 17:33:51 -0500

    prioridad

--- NexusGtics/src/main/java/com/example/nexusgtics/entity/Ticket.java
+++ NexusGtics/src/main/java/com/example/nexusgtics/entity/Ticket.java
@@ -55,4 +55,6 @@ public class Ticket {
     @JoinColumn(name = "idTipoTicket", nullable = false)
     private Tipoticket idTipoTicket;
 
+    @Column(name = "prioridad", length = 45)
+    private String prioridad;
 }
