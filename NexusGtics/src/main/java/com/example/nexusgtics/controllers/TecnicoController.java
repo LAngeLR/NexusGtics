810e3606f9dbd40fa7397e24545fca397a06de1a
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-09-15 01:46:17 -0500

    tecnico - un crud sin caerse listar

--- NexusGtics/src/main/java/com/example/nexusgtics/controllers/TecnicoController.java
+++ NexusGtics/src/main/java/com/example/nexusgtics/controllers/TecnicoController.java
@@ -1,19 +1,55 @@
 package com.example.nexusgtics.controllers;
+import com.example.nexusgtics.entity.Formulario;
+import com.example.nexusgtics.entity.Ticket;
+import com.example.nexusgtics.entity.Tipoticket;
+import com.example.nexusgtics.entity.Usuario;
+import com.example.nexusgtics.repository.TicketRepository;
+import com.example.nexusgtics.repository.TipoticketRepository;
+import com.example.nexusgtics.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/tecnico")
 public class TecnicoController {
+    final TicketRepository ticketRepository;
+    final UsuarioRepository usuarioRepository;
+    private final TipoticketRepository tipoticketRepository;
+
+    public TecnicoController(TicketRepository ticketRepository,
+                             UsuarioRepository usuarioRepository,
+                             TipoticketRepository tipoticketRepository){
+        this.ticketRepository = ticketRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.tipoticketRepository = tipoticketRepository;
+    }
     @GetMapping("/")
     public String paginaPrincipal(){
         return "Tecnico/tecnico";
     }
     @GetMapping("/perfiltecnico")
-    public String perfilTecnico(){
-        return "Tecnico/perfiltecnico";
+    public String perfilTecnico(Model model, @RequestParam("id") int id){
+
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if(optUsuario.isPresent()){
+            Usuario usuario = optUsuario.get();
+            model.addAttribute("usuario",usuario);
+            return "Tecnico/perfiltecnico";
+        }else {
+            return "redirect:/Tecnico/perfiltecnico";
+        }
     }
+
 
     @GetMapping("/comentarios")
     public String pagcomentarios(){
@@ -26,8 +62,19 @@ public class TecnicoController {
     }
 
     @GetMapping("/datosticket")
-    public String pagdatostick(){
-        return "Tecnico/datos_ticket";
+    public String pagdatostick(Model model){
+        Ticket ticket = new Ticket();
+        Tipoticket tipoticket = new Tipoticket();
+        model.addAttribute("ticket",ticket);
+        model.addAttribute("tipoticket",tipoticket);
+
+        //'listar'
+       // List<Ticket> lista = ticketRepository.findAll();
+      //  model.addAttribute("ticket", lista);
+      // List<Tipoticket> list = tipoticketRepository.findAll();
+      //model.addAttribute("tipoticket", list);
+            return "Tecnico/datos_ticket";
+
     }
 
     @GetMapping("/desplazamiento")
@@ -35,23 +82,36 @@ public class TecnicoController {
         return "Tecnico/desplazamiento";
     }
 
-    @GetMapping("/editarperfil")
-    public String pageditarperfil(){
-        return "Tecnico/editar_perfil";
-    }
-
     @GetMapping("/formulario")
-    public String pagformulario(){
+    public String pagformulario(Model model){
+        List<Ticket> lista = ticketRepository.findAll();
+        model.addAttribute("ticketList", lista);
+
+        List<Usuario> list = usuarioRepository.findAll();
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuarioList", list);
+        model.addAttribute("usuario", usuario);
         return "Tecnico/formulario";
     }
+
+    @PostMapping("/save")
+    public String guardarFormulario(Ticket ticket, RedirectAttributes attr){
+        ticketRepository.save(ticket);
+        return "redirect:/Tecnico/datos_ticket";
+    }
+
 
     @GetMapping("/mapa")
     public String pagmapa(){
         return "Tecnico/mapa";
     }
 
+    //YA ESTA CRUD LISTAR
     @GetMapping("/ticketasignado")
-    public String pagtickasignado(){
+    public String pagtickasignado(Model model){
+        //'listar'
+        List<Ticket> lista = ticketRepository.findAll();
+        model.addAttribute("ticketList", lista);
         return "Tecnico/ticket_asignado";
     }
 
