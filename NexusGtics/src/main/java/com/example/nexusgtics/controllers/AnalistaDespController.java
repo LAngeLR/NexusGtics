03fccf23baf5e4e54092596b79ecbda328e45224
David Llanos <david.llanos@pucp.edu.pe>
2023-09-15 04:06:08 -0500

    Nose que le falta a mi editar :c

--- NexusGtics/src/main/java/com/example/nexusgtics/controllers/AnalistaDespController.java
+++ NexusGtics/src/main/java/com/example/nexusgtics/controllers/AnalistaDespController.java
@@ -5,9 +5,12 @@ import com.example.nexusgtics.repository.SitioRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/analistaDespliegue")
@@ -36,9 +39,25 @@ public class AnalistaDespController {
     }
 
     @GetMapping("/editarSitio")
-    public String editarSitio(){
-        return "AnalistaDespliegue/despliegueEditarSitio";
+    public String editarSitio(Model model, @RequestParam("id") int id){
+        Optional<Sitio> optSitio = sitioRepository.findById(id);
+        if(optSitio.isPresent()){
+            Sitio sitio = optSitio.get();
+            model.addAttribute("sitio", sitio);
+            return "AnalistaDespliegue/despliegueEditarSitio";
+        }else {
+            return "redirect: /AnalistaDespliegue/despliegueListaSitio";
+        }
+
     }
+
+    @PostMapping("/guardarSitio")
+    public String guardarSitio(Sitio sitio){
+        sitioRepository.save(sitio);
+        return "redirect:/AnalistaDespliegue/despliegueListaSitio";
+    }
+
+
     @GetMapping("/listaEquipo")
     public String listaEquipo(){
         return "AnalistaDespliegue/despliegueListaEquipos";
