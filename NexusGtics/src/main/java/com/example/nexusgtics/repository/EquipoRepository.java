705ab37e2483f6357b30022fcd99a04379628270
Oscar <a20193315@pucp.edu.pe>
2023-09-21 04:19:54 -0500

    avance admin, falta concluir

--- NexusGtics/src/main/java/com/example/nexusgtics/repository/EquipoRepository.java
+++ NexusGtics/src/main/java/com/example/nexusgtics/repository/EquipoRepository.java
@@ -11,6 +11,14 @@ public interface EquipoRepository extends JpaRepository<Equipo, Integer> {
 
     @Transactional
     @Modifying
-    @Query(nativeQuery = true, value = "insert into equipos (marca, modelo, descripcion, paginaModelo, idSitios, idTipoEquipo, habilitado) values (?1, ?2, ?3, ?4, ?5, ?6, true)")
-    void guardarEquipo(String marca, String modelo, String descripcion, String paginaModelo, int idSitios, int idTipoEquipo);
+    @Query(nativeQuery = true, value = "insert into equipos (marca, modelo, descripcion, paginaModelo, idSitios, idTipoEquipo, idImagenes, habilitado) values (?1, ?2, ?3, ?4, ?5, ?6, ?7, true)")
+    void guardarEquipo(String marca, String modelo, String descripcion, String paginaModelo, int idSitios, int idTipoEquipo, int idImagenes);
+
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "update equipos set marca = ?1, modelo = ?2, descripcion = ?3, paginaModelo = ?4, idSitios = ?5, idTipoEquipo = ?6, idImagenes = ?7, habilitado = true where idEquipos = ?8")
+    void actualizarEquipo(String marca, String modelo, String descripcion, String paginaModelo, int idSitios, int idTipoEquipo, int idImagenes, int idEquipos);
+
 }
--- NexusGtics/src/main/java/com/example/nexusgtics/repository/SitioRepository.java
+++ NexusGtics/src/main/java/com/example/nexusgtics/repository/SitioRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface SitioRepository extends JpaRepository<Sitio, Integer> {
     @Transactional
     @Modifying
-    @Query(nativeQuery = true, value = "insert into sitios (departamento, provincia, distrito, ubigeo, latitud, longitud, tipo, tipoZona, habilitado) values (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, true)")
-    void guardarSitio(String departamento, String provincia, String distrito, Integer ubigeo, BigDecimal latitud, BigDecimal longitud, String tipo, String tipoZona);
+    @Query(nativeQuery = true, value = "insert into sitios (departamento, provincia, distrito, ubigeo, latitud, longitud, tipo, tipoZona, idArchivos, habilitado) values (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, true)")
+    void guardarSitio(String departamento, String provincia, String distrito, Integer ubigeo, BigDecimal latitud, BigDecimal longitud, String tipo, String tipoZona, int idArchivos);
 
 }
