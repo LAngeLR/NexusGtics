cb7c1cd1976c5ff30d231a8c7bbec79d23b7b39d
Anaís Salvador <anaissalvadorrodriguez@gmail.com>
2023-12-06 19:20:43 -0500

    aun falta editar, no entra a ver tickets

---
+++ NexusGtics/src/main/java/com/example/nexusgtics/controllers/borrador.java
@@ -0,0 +1,26 @@
+// Obtener la cantidad total de tickets
+
+
+        /*int totalTickets = ticketRepository.cantidadTickets();
+                model.addAttribute("totalTickets", totalTickets);
+
+                // Obtener la cantidad total de tickets nuevos
+                int ticketsnuevos = ticketRepository.cantidadTicketsnuevos();
+                model.addAttribute("totalTicketsNuevos", ticketsnuevos);
+
+                // Obtener la cantidad total de empresas nuevos
+                int totalEmpresas = ticketRepository.cantidadEmpresas();
+                model.addAttribute("totalempresas", totalEmpresas);
+
+                // Obtener la cantidad total de empresas nuevos
+                int totalCuadrillas = ticketRepository.cantidadCuadrilla();
+                model.addAttribute("totalCuadrillas", totalCuadrillas);
+
+                // Obtener la lista de tickets
+                List<Ticket> listaT = ticketRepository.listaTicketsAsignado(u.getIdSupervisorEncargado().getId(),u.getIdCuadrilla().getIdCuadrillas(),u.getIdEmpresaAsignada().getIdEmpresas());
+        model.addAttribute("listaTicket", listaT);
+
+        // Obtener la lista de tickets por estado
+        List<Ticket> lista = ticketRepository.listarEstado();
+        model.addAttribute("ticketList", lista);
+        return "Tecnico/dashboard";*/
